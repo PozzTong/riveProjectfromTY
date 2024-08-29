d2f470105709aee5edec8a9faf75d8933bf6e851
Pozz Tong <pozztong12800900@gmail.com>
2024-08-29 16:42:01 +0700

    push

--- lib/home_screen/page_03.dart
+++ lib/home_screen/page_03.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../pro2/core/app/app.dart';
 import '../pro2/core/theme/sh_color.dart';
+import '../pro3/pro3.dart';
 
 class PageThree extends StatefulWidget {
   const PageThree({super.key});
@@ -16,26 +17,51 @@ class _PageThreeState extends State<PageThree> {
     return Scaffold(
       // appBar: AppBar(title: Text('Pro2'),),
         body: SafeArea(
-      child: Center(
-        child: GestureDetector(
-          child: Container(
-              height: 50,
-              width: 70,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: SHColors.cardColor),
-              child: const Center(
-                  child: Text(
-                'Home',
-                style: TextStyle(color: Colors.white),
-              ))),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const SmartHomeApp()));
-          },
-        ),
+      child: Column(
+        children: [
+          Center(
+            child: GestureDetector(
+              child: Container(
+                  height: 50,
+                  width: 70,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: SHColors.cardColor),
+                  child: const Center(
+                      child: Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ))),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SmartHomeApp()));
+              },
+            ),
+          ),
+          Center(
+            child: GestureDetector(
+              child: Container(
+                  height: 50,
+                  width: 70,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: SHColors.cardColor),
+                  child: const Center(
+                      child: Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ))),
+              onTap: () {
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => VibratingApp() ));
+              },
+            ),
+          ),
+        ],
       ),
     ));
   }
